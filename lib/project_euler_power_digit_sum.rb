8e75075157222d8f3d503c0ca00411964e31a20c
gncurran <gnc115@gmail.com>
2015-12-06 14:32:22 -0500

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,7 @@
-# code your solution here
+# Write a method that takes a base (x) and an exponent (n) as arguments and returns the sum of the digits of x raised to the power of n.
+def power_digit_sum(x, n)
+  powered = x**n
+  split = powered.to_s.chars.map(&:to_i)
+  final = split.inject(:+)
+  return final
+end
